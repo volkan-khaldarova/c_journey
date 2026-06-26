3b17a03887cd522754f105b05c678d094af5f558
volkan <volkannecipoglu@proton.me>
2026-06-26 11:20:03 +0200

    implement ex06.c

---
+++ C-programming/ch02/exercises/ex06/ex06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int i1, i2, i3;
+  float f1, f2, f3;
+
+  printf("int value: %d %d %d\n", i1, i2, i3);
+  printf("float value: %f %f %f\n", f1, f2, f3);
+
+  return 0;
+}
