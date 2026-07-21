dce83abe5f681a56a33d84b8c6f73cf5fa2d1188
volkan <volkannecipoglu@proton.me>
2026-07-21 20:41:25 +0200

    Complete phone number translation exercise using switch-case

---
+++ C-programming/ch07/projects/p04/p04.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char ch;
+
+  printf("Enter phone number: ");
+  ch = getchar();
+  while (ch != '\n') {
+    switch (ch) {
+    case 'A':
+    case 'B':
+    case 'C':
+      printf("2");
+      break;
+    case 'D':
+    case 'E':
+    case 'F':
+      printf("3");
+      break;
+    case 'G':
+    case 'H':
+    case 'I':
+      printf("4");
+      break;
+    case 'J':
+    case 'K':
+    case 'L':
+      printf("5");
+      break;
+    case 'M':
+    case 'N':
+    case 'O':
+      printf("6");
+      break;
+    case 'P':
+    case 'R':
+    case 'S':
+      printf("7");
+      break;
+    case 'T':
+    case 'U':
+    case 'V':
+      printf("8");
+      break;
+    case 'W':
+    case 'X':
+    case 'Y':
+      printf("9");
+      break;
+    default:
+      putchar(ch);
+      break;
+    }
+    ch = getchar();
+  }
+
+  printf("\n");
+
+  return EXIT_SUCCESS;
+}
